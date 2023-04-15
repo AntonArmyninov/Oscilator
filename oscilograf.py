88c9630210c88ecb97af8117c278244f0a5af017
AntonArmyninov <armyninovbleide@gmail.com>
2023-04-15 20:18:34 +0500

    change Oscillator

--- oscilograf.py
+++ oscilograf.py
@@ -1,16 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-A = 1         # амплитуда
-ω = 2 * np.pi # угловая частота
-φ = 0         # начальная фаза
+# Осцилограф
+def f(t):
+    A, w, u = 1, 2 * np.pi, 0 #задаем амплитуду, угловую частоту и начальную фазу
+    length = t.shape[0]
+    x = A * np.cos(w * t + u)
+    return x
 
-t_start, t_end, dt = 0, 2, 0.01 # начало, конец и шаг по времени
-t = np.arange(t_start, t_end, dt) # массив временных отсчетов
+def gaus(t):
+    length = t.shape[0]  # длина массива t
+    u, o = 0, 0.05
+    noise = np.random.normal(u, o, size=length)
+    return noise
 
-x = A * np.cos(ω * t + φ) # вычисление координаты для каждого временного отсчета
 
-plt.plot(t, x) # построение графика
+t= np.linspace(0, 2, 100) #шаг графика
+
+#x = A * np.cos(w * t + u) # вычисление координаты для каждого временного отсчета
+
+plt.plot(t, f(t)) # построение графика
 plt.xlabel('время') # подпись по оси x
 plt.ylabel('координата') # подпись по оси y
 plt.show() # вывод графика на экран
