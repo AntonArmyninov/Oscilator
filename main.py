a3e6200581a8b6b02cf1410bdf7947e959a8f1bf
AntonArmyninov <armyninovbleide@gmail.com>
2023-04-15 18:48:48 +0500

    gaussian

--- main.py
+++ main.py
@@ -9,15 +9,24 @@ def s(k):
     signal = (k > randoms).astype(float) # создание сигнала на основе сравнения k и randoms
     return signal
 
+#Гаусовский шум
+def n(k):
+    length = k.shape[0]  # длина массива k
+    u, o = 0, 0.05
+    noise = np.random.normal(u, o, size=length)
+
+    return noise
+
 #создание массива k, который начинается с -1 и заканчивается на 1, включая 100 равных промежутков
 k = np.linspace(-1, 1, 100)
 
 #создание графика
 fig, ax = plt.subplots(figsize=(16, 9)) # создание контейнера для графика с определенным размером
 ax.plot(k, s(k), label='s(k)') # добавление графика для сигнала s(k)
+ax.plot(k, s(k)+n(k), label='s(k)+n(k)')
 ax.set_xlabel('k') # добавление подписи для оси x
 ax.set_ylabel('v') # добавление подписи для оси y
-ax.set_title('Непериодический сигнал s(k)') # добавление заголовка графика
+ax.set_title('Гаусовский шум s(k)') # добавление заголовка графика
 ax.legend(); # добавление легенды
 
 plt.show() # вывод графика на экран
